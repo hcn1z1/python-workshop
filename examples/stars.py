3e0fe1f33037a53a606f8f72e30734b7904561fd
HCn1 <58739097+hcn1z1@users.noreply.github.com>
2022-03-24 20:41:14 -0700

    Create stars.py

---
+++ examples/stars.py
@@ -0,0 +1,24 @@
+n = int(input("Entre un nbr "))
+
+stars = 1
+space = n//2
+while space>=0:
+    print(" "*space + "*"*stars)
+    stars = stars + 2
+    space = space -1
+
+
+space = n//2
+for x in range(n-1):
+    print(" "*space+"*")
+    
+'''
+example of output / n = 5
+  *
+ ***
+*****
+  *
+  *
+  *
+  *
+'''
